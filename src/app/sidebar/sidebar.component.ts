888de23b7be211e142a3a84b37f49b5480abebd0
El Hadji Ousseynou DJITE [SNT DRPS/TPP/STN] <ext_djite004930@orange-sonatel.com>
2024-05-21 11:53:59 +0000

    menu added

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,113 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import PerfectScrollbar from 'perfect-scrollbar';
+import { Account } from '../models/account';
+import { Router } from '@angular/router';
+import { BaseService } from '../shared/base.service';
+import { AuthService } from '../pages/auth/auth.service';
+import { ActionGroup } from '../models/actionGroup';
+import { RouteInfos } from '../models/routeInfo';
+import { group } from '@angular/animations';
 
 declare const $: any;
 
-//Metadata
+// Metadata
 export interface RouteInfo {
     path: string;
     title: string;
+    visible: boolean;
     type: string;
     icontype: string;
     collapse?: string;
+    index?: number,
     children?: ChildrenItems[];
 }
 
 export interface ChildrenItems {
     path: string;
+    visible: boolean;
     title: string;
     ab: string;
     type?: string;
 }
 
-//Menu Items
-export const ROUTES: RouteInfo[] = [{
-        path: '/dashboard',
-        title: 'Dashboard',
-        type: 'link',
-        icontype: 'dashboard'
-    },{
-        path: '/components',
-        title: 'Components',
-        type: 'sub',
-        icontype: 'apps',
-        collapse: 'components',
-        children: [
-            {path: 'buttons', title: 'Buttons', ab:'B'},
-            {path: 'grid', title: 'Grid System', ab:'GS'},
-            {path: 'panels', title: 'Panels', ab:'P'},
-            {path: 'sweet-alert', title: 'Sweet Alert', ab:'SA'},
-            {path: 'notifications', title: 'Notifications', ab:'N'},
-            {path: 'icons', title: 'Icons', ab:'I'},
-            {path: 'typography', title: 'Typography', ab:'T'}
-        ]
-    },{
-        path: '/forms',
-        title: 'Forms',
-        type: 'sub',
-        icontype: 'content_paste',
-        collapse: 'forms',
-        children: [
-            {path: 'regular', title: 'Regular Forms', ab:'RF'},
-            {path: 'extended', title: 'Extended Forms', ab:'EF'},
-            {path: 'validation', title: 'Validation Forms', ab:'VF'},
-            {path: 'wizard', title: 'Wizard', ab:'W'}
-        ]
-    },{
-        path: '/tables',
-        title: 'Tables',
-        type: 'sub',
-        icontype: 'grid_on',
-        collapse: 'tables',
-        children: [
-            {path: 'regular', title: 'Regular Tables', ab:'RT'},
-            {path: 'extended', title: 'Extended Tables', ab:'ET'},
-            {path: 'datatables.net', title: 'Datatables.net', ab:'DT'}
-        ]
-    },{
-        path: '/maps',
-        title: 'Maps',
-        type: 'sub',
-        icontype: 'place',
-        collapse: 'maps',
-        children: [
-            {path: 'google', title: 'Google Maps', ab:'GM'},
-            {path: 'fullscreen', title: 'Full Screen Map', ab:'FSM'},
-            {path: 'vector', title: 'Vector Map', ab:'VM'}
-        ]
-    },{
-        path: '/widgets',
-        title: 'Widgets',
-        type: 'link',
-        icontype: 'widgets'
+export let ROUTES = [];
 
-    },{
-        path: '/charts',
-        title: 'Charts',
-        type: 'link',
-        icontype: 'timeline'
-
-    },{
-        path: '/calendar',
-        title: 'Calendar',
-        type: 'link',
-        icontype: 'date_range'
-    },{
-        path: '/pages',
-        title: 'Pages',
-        type: 'sub',
-        icontype: 'image',
-        collapse: 'pages',
-        children: [
-            {path: 'pricing', title: 'Pricing', ab:'P'},
-            {path: 'timeline', title: 'Timeline Page', ab:'TP'},
-            {path: 'login', title: 'Login Page', ab:'LP'},
-            {path: 'register', title: 'Register Page', ab:'RP'},
-            {path: 'lock', title: 'Lock Screen Page', ab:'LSP'},
-            {path: 'user', title: 'User Page', ab:'UP'}
-        ]
-    }
-];
 @Component({
     selector: 'app-sidebar-cmp',
     templateUrl: 'sidebar.component.html',
@@ -115,7 +39,128 @@ export const ROUTES: RouteInfo[] = [{
 
 export class SidebarComponent implements OnInit {
     public menuItems: any[];
-    ps: any;
+    actionsGroup: ActionGroup[] = [];
+
+    account: Account = new Account();
+    userMenuItem : RouteInfos[] = []; 
+    constructor(private router: Router, private baseService: BaseService, public authService: AuthService) { 
+    }
+
+    ngOnInit() {
+        this.account = this.authService.getCurrentAccount();
+        if (this.authService.isLoggedIn()) {
+            this.actionsGroup.unshift({
+                titre: 'DALAL AK JAAM JAMBAR',
+                path: '/mfe/home',
+                type: 'link',
+                icon: 'dashboard',
+                //visible: true
+            });
+            // console.log(this.actionsGroup);
+            this.getActionsGroup( res => {
+                this.getSideBarItem(res => {
+                    // order menuItem
+                    this.userMenuItem.sort((a,b) => (a.index < b.index) ? 1 : ((b.index < a.index) ? -1 : 0));
+                    this.menuItems = this.userMenuItem.filter(menuItem => menuItem);
+                });
+            });
+        }
+    }
+    getSideBarItem(cb) {
+       this.actionsGroup.forEach(actionGroup => {
+           actionGroup.actions.forEach(action => {
+               if (this.isPresent(action.roles)) {
+                   this.addMenuItem(actionGroup, action)
+               } 
+           });
+           return cb(1);
+       });
+    }
+
+    addMenuItem(groupe, action) {
+        let currentGroup = this.userMenuItem.find(uMI => uMI.title === groupe.titre);
+        if (currentGroup) {
+            let currentChild = currentGroup.children.find(cC => cC.path === action.path)
+            if (!currentChild)
+                currentGroup.children.push({path: action.path, title: action.titre, ab: action.ab})
+        }else {
+            this.userMenuItem.push({
+                path: groupe.path,
+                title: groupe.titre,
+                type: groupe.type,
+                icontype: groupe.icon,
+                collapse: groupe.collapse,
+                children: [
+                    {path: action.path, title: action.titre, ab: action.ab}
+                ]
+            })
+        }
+    }
+    isPresent(roles) {
+        let userRoles = this.authService.getAccountRoles();
+        let present = false;
+        let i = 0;
+        while ( !present && i < userRoles.length) {
+            present = roles.find(r => r && r.name === userRoles[i]);
+            i++;
+        }
+        return present;
+    }
+
+    getActionsGroup(cb){
+    this.baseService.get('/ActionGroups?filter={"include":["actions"]}',true)
+      .subscribe(
+        res => {
+          this.actionsGroup = res;
+          // console.log('Action Groups:', res);
+          this.actionsGroup.unshift({
+            titre: 'DALAL AK JAAM JAMBAR',
+            path: '/home',
+            type: 'link',
+            icon: 'dashboard'
+          });
+          this.actionsGroup.forEach(actionGroup => {
+            actionGroup.actions.forEach(action => {
+                action.roles = [];
+                this.baseService.get('/ActionRoles?filter={"where":{"actionId":"'+action.id+'"}, "include":"role"}',false)
+                .subscribe(
+                  res => {
+                      res.forEach(actionRole => {
+                        action.roles.push(actionRole.role);
+                      });
+                    return cb(1);
+                  },
+                  err => {
+                    console.log (err);
+                  }
+                )
+            });
+          });
+          console.log(this.actionsGroup)
+        },
+        err => {
+          console.log(err);
+        }
+      )
+    }
+    isUserLoggedIn(): boolean {
+        // Insérez ici la logique pour vérifier si l'utilisateur est connecté
+        return this.authService.isLoggedIn(); // Par exemple, si vous utilisez un service d'authentification
+    }
+
+    logout(){
+        this.baseService.post("Accounts/logout", true,{accessTokenID: this.authService.getToken()})
+        .subscribe(
+          res => {
+            // redirect to login
+            this.router.navigate(['/pages/login']);
+          },
+          err => {
+            console.log(err);
+          }
+        )
+        this.authService.logout();
+      }
     isMobileMenu() {
         if ($(window).width() > 991) {
             return false;
@@ -123,16 +168,10 @@ export class SidebarComponent implements OnInit {
         return true;
     };
 
-    ngOnInit() {
-        this.menuItems = ROUTES.filter(menuItem => menuItem);
-        if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
-            const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
-            this.ps = new PerfectScrollbar(elemSidebar);
-        }
-    }
     updatePS(): void  {
         if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
-            this.ps.update();
+            const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
+            let ps = new PerfectScrollbar(elemSidebar, { wheelSpeed: 2, suppressScrollX: true });
         }
     }
     isMac(): boolean {
@@ -141,11 +180,5 @@ export class SidebarComponent implements OnInit {
             bool = true;
         }
         return bool;
-    }
-    expandOrCollapseMenu(id){
-        let parent = document.getElementById(id+ "-p");
-        let child = document.getElementById(id);
-        parent.ariaExpanded = parent.ariaExpanded === "true" ? "false" : "true";
-        child.style.height = child.style.height === "0px" || child.style.height === ""  ? "100%" : "0";
     }
 }
--- src/app/sidebar/sidebar.module.ts
+++ src/app/sidebar/sidebar.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
+
 
 @NgModule({
     imports: [ RouterModule, CommonModule ],
