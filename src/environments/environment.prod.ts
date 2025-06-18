ed820e59416da073b4f7fb8aad8e39026e755415
Fatima <fatimseck54@gmail.com>
2025-06-18 21:39:30 +0000

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   metricPath: "https://metric.jambars.orange-sonatel.com/",
   // serverURLMETRIC: "/api/metric/",
   serverURLMETRIC : "http://127.0.0.1:3009/",
-   serverURLjambar: "https://voix-marins-backend-1.onrender.com/",
+   serverURLjambar: "https://gaalgui.sn/",
  // serverURLjambar: "https://7a28-41-83-96-192.ngrok-free.app/",
   
   serverURLjambarlocale:"http://localhost:3001/api/",
